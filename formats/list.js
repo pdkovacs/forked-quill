d40d846ba79faea8d298debf0641d8c6c1e27b2b
Kovács, Péter <peter.dunay.kovacs@gmail.com>
2019-12-27 22:55:54 +0100

    fix: List item head sizes out of synch - 2856

--- formats/list.js
+++ formats/list.js
@@ -40,11 +40,75 @@ class ListItem extends Block {
     this.attachUI(ui);
   }
 
+  insertBefore(childBlot, refBlot) {
+    super.insertBefore(childBlot, refBlot);
+    if (childBlot.attributes && childBlot.attributes.values().size) {
+      const nonDefaultSize = childBlot.attributes.values().size;
+      this.evaluateSizeAttributor(nonDefaultSize);
+    }
+  }
+
   format(name, value) {
     if (name === this.statics.blotName && value) {
       this.domNode.setAttribute('data-list', value);
     } else {
       super.format(name, value);
+    }
+  }
+
+  formatAt(index, length, name, value) {
+    super.formatAt(index, length, name, value);
+
+    if (name === 'size') {
+      if (this.isSameSize(value)) {
+        this.evaluateSizeAttributor(value);
+      }
+    }
+  }
+
+  isSameSize(size) {
+    let sameSize = true;
+    let hasDefaultValue = false;
+    let hasNonDefaultValue = false;
+
+    this.children.forEach(child => {
+      if (!child.attributes) {
+        hasDefaultValue = true;
+      } else if (!child.attributes.values().size) {
+        hasDefaultValue = true;
+      } else {
+        hasNonDefaultValue = true;
+        sameSize = child.attributes.values().size === size && sameSize;
+      }
+    });
+    if (hasDefaultValue && hasNonDefaultValue) {
+      return false;
+    }
+    return sameSize;
+  }
+
+  evaluateSizeAttributor(value) {
+    const cls = Quill.import('attributors/class/size');
+    const sty = Quill.import('attributors/style/size');
+    const sizeAttor = Quill.import('formats/size');
+    if (cls && cls.whitelist && cls.whitelist.indexOf(value) > -1) {
+      this.uiNode.classList.forEach(token => {
+        if (token.startsWith('ql-size-')) {
+          this.uiNode.classList.remove(token);
+        }
+      });
+      this.uiNode.classList.add(`${cls.keyName}-${value}`);
+    } else if (sty && sty.whitelist && sty.whitelist.indexOf(value) > -1) {
+      this.uiNode.setAttribute('style', `font-size: ${value}`);
+    } else if (value) {
+      console.warn('!!!!!!!! no size attributor found');
+    } else if (sizeAttor === cls) {
+      this.uiNode.removeAttribute('class');
+      this.attachUI(this.uiNode);
+    } else if (sizeAttor === sty) {
+      this.uiNode.removeAttribute('style');
+    } else {
+      console.warn('!!!!!!!! no size attributor found');
     }
   }
 }
